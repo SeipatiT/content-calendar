25a710e151bb789dc95fe22df2427fae9907e6f5
SeipatiT <109255052+SeipatiT@users.noreply.github.com>
2024-02-14 16:56:46 +0200

    Initialized Vite.js frontend server and successfully pulled data in JSON format from Spring App.

--- content-calendar/src/main/java/com/gitprojects/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/com/gitprojects/contentcalendar/controller/ContentController.java
@@ -3,15 +3,18 @@ package com.gitprojects.contentcalendar.controller;
 import com.gitprojects.contentcalendar.model.Content;
 import com.gitprojects.contentcalendar.repository.ContentCollectionRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
+//@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ContentController {
     private ContentCollectionRepository repository;
     public ContentController(ContentCollectionRepository repository) {
@@ -31,9 +34,29 @@ public class ContentController {
                 new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Content not found"));
     }
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content){
+    public void create(@Valid @RequestBody Content content){
             repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Content not found");
+        }   repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping ("/{id}")
+    public void delete(@PathVariable Integer id){
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Content not found");
+        }
+        repository.deleteById(id);
     }
 
 //    @PostConstruct
--- content-calendar/src/main/java/com/gitprojects/contentcalendar/model/Content.java
+++ content-calendar/src/main/java/com/gitprojects/contentcalendar/model/Content.java
@@ -1,14 +1,19 @@
 package com.gitprojects.contentcalendar.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.sql.Statement;
 import java.time.LocalDateTime;
 
 public record Content(
        Integer id,
+       @NotBlank
        String title,
        String desc,
        Status status,
        Type contentType,
+
        LocalDateTime dateCreated,
        LocalDateTime dateUpdated,
        String url
